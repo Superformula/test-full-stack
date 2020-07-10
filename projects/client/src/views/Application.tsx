4c74977c1574a9ae6f2e710e17fc47bbf71eea35
Matthew Evers <mrobertevers+1@gmail.com>
2020-07-09 21:08:32 -0500

    Add "User List" view CSS to common.css
    FIX:  minor bug in webpack pipeline for CSS discovery
    TEMPORARY: Demo html in Application.tsx for "User List" css

--- projects/client/src/views/Application.tsx
+++ projects/client/src/views/Application.tsx
@@ -2,9 +2,51 @@ import * as React from 'react';
 
 const Application: React.FunctionComponent = () => {
     return (
-        <h1>
-            Hello
-        </h1>
+        <div className="application-container">
+            <div className="application-container-style">
+                <div className="application-header">
+                    <div className="user-card-grid-element">
+                        <h1 className="application-header-title">Users list</h1>
+                    </div>
+                    <div className="user-card-grid-element">
+                        <div style={{flexGrow: 1}}></div>
+                    </div>
+                    <div className="user-card-grid-element">
+                        <input className="search-input" type="text" placeholder="Search..."></input>
+                    </div>
+                </div>
+                <div className="infinite-scroll-box">
+                    <div className="infinite-scroll-box-inner">
+                        <div className="user-card-grid-element user-card-grid-element-row-element">
+                            <div className="user-card">This is text</div>
+                        </div>
+                        <div className="user-card-grid-element user-card-grid-element-row-element">
+                            <div className="user-card">This is text</div>
+                        </div>
+                        <div className="user-card-grid-element user-card-grid-element-row-element">
+                            <div className="user-card">This is text</div>
+                        </div>
+                        <div className="user-card-grid-element user-card-grid-element-row-element">
+                            <div className="user-card">This is text</div>
+                        </div>
+                        <div className="user-card-grid-element user-card-grid-element-row-element">
+                            <div className="user-card">This is text</div>
+                        </div>
+                        <div className="user-card-grid-element user-card-grid-element-row-element">
+                            <div className="user-card">This is text</div>
+                        </div>
+                        <div className="user-card-grid-element user-card-grid-element-row-element">
+                            <div className="user-card">This is text</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="infinite-scroll-box-load">
+                    <div className="load-more-button-container">
+                        <button className="load-more-button" disabled={true}>Load More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- projects/client/tools/webpack/configs/webpack-dev.config.js
+++ projects/client/tools/webpack/configs/webpack-dev.config.js
@@ -58,7 +58,7 @@ module.exports = {
             {
                 test: /\.css$/,
                 loader: 'style-loader!css-loader',
-                include: [path.join(__dirname, 'src')]
+                include: [sourceDirectory]
             },
             {
                 test: /.*\.(gif|png|jpeg|svg|mp4)$/i,
