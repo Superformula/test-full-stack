bbbe6befb93b889c86c75d18ec4323d17fab69fe
tommyc <tkachan@gmail.com>
2020-07-12 01:14:57 -0400

    Delete environment.js

--- site/cypress/support/environment.js
+++
@@ -1,5 +0,0 @@
-process.env.REACT_APP_AWS_APP_SYNC_GRAPHQL_URL =
-  "https://2pwaved4fff6zh4nzyrgi4z62q.appsync-api.us-east-1.amazonaws.com/graphql";
-process.env.REACT_APP_AWS_APP_SYNC_REGION = "us-east-1";
-process.env.REACT_APP_AWS_APP_SYNC_API_KEY = "da2-fpqy75egtffnjc5wfinbhsu6pa";
-process.env.REACT_APP_USERS_NUMBER_LIMIT = 6;
