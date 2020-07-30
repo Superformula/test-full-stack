6ed7cb913a5cec152e607af7854aa74a346902e3
Victor Silva <vpcx5@yahoo.com.br>
2020-07-30 13:50:17 -0300

    feat: 🎸 Define lambda ts interfaces

---
+++ api/src/interfaces/index.ts
@@ -0,0 +1,56 @@
+export interface IUser {
+  id?: string;
+  name?: string;
+  dob?: string;
+  address?: string;
+  description?: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface ILoggerConfig {
+  name?: string;
+  level?: string;
+  enabled?: string;
+}
+
+export interface ILocationInformation {
+  center: number[];
+  geometry: IMapBoxGeometry;
+  context: IMapBoxContext;
+}
+
+export interface IMapBoxGeometry {
+  type: string;
+  coordinates: number[];
+}
+
+export interface IMapBoxContext {
+  id: string;
+  text: string;
+  short_code?: string;
+}
+
+export interface IMapBoxFeature {
+  id: string;
+  type: string;
+  place_type: string[];
+  relevance: number;
+  text: string;
+  place_name?: string;
+  matching_place_name?: string;
+  center: number[];
+  geometry: IMapBoxGeometry;
+  context: IMapBoxContext;
+}
+
+export interface IMapBoxResponse {
+  type: string;
+  query: string[];
+  features: IMapBoxFeature[];
+  attribution: string;
+}
+
+export interface IMapBoxErrorResponse {
+  message: string;
+}
