3ecdee26b73842da93d2c0731a0d2d953de06564
Lance Gundersen <53791851+linuxlance@users.noreply.github.com>
2020-08-03 16:13:53 -0400

    Add load more functionality
    - show and hide the button based on nextToken
    - trigger query on page load

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import "./assets/App.scss";
-import Amplify, { graphqlOperation } from "aws-amplify";
-import { Connect } from "aws-amplify-react";
+import Amplify, { graphqlOperation, API } from "aws-amplify";
+import retry from "async-retry";
 // import _ from "lodash";
 import SearchInput from "./components/SearchInput";
-import LoadMoreButton from "./components/LoadMoreButton";
 import Card from "./components/Card";
 import aws_exports from "./aws-exports";
 import { User } from "./models";
@@ -12,8 +11,8 @@ Amplify.configure(aws_exports);
 Amplify.Logger.LOG_LEVEL = "INFO";
 
 const ListUsers = `
-query ListUsers {
-    listUsers(limit: 6) {
+query ListUsers($limit: Int, $nextToken: String) {
+    listUsers(limit: $limit, nextToken: $nextToken) {
         nextToken
         items {
           id
@@ -26,7 +25,65 @@ query ListUsers {
     }
 }`;
 
-class App extends Component {
+const GqlRetry = async (query: any, variables?: any) => {
+  return await retry(
+    async () => {
+      console.log("Sending GraphQL operation", {
+        query: query,
+        vars: variables,
+      });
+      const response = await API.graphql(graphqlOperation(query, variables));
+      console.log("GraphQL result", {
+        result: response,
+        query: query,
+        vars: variables,
+      });
+      return response;
+    },
+    {
+      retries: 10,
+    }
+  );
+};
+
+class App extends Component<{}, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      nextToken: "",
+      users: [],
+    };
+  }
+
+  async componentDidMount() {
+    this.getUsers();
+}
+
+  setNextToken = (token: String) => {
+    if (token) this.setState({ nextToken: token });
+  };
+
+  handleAddUsers = (givenUsersList: any[]) => {
+    const usersList = this.state.users.concat(givenUsersList);
+    this.setState({ users: usersList });
+  };
+
+  getUsers = async () => {
+    // TODO: Find the type for the graphql operation response
+    await GqlRetry(ListUsers, { limit: 6 }).then(({data}: any) => {
+      this.setState({ users: data.listUsers.items });
+      this.setState({ nextToken: data.listUsers.nextToken });
+    });
+  };
+
+  getNextUsers = async () => {
+    // TODO: Integrate with getUsers to keep DRY
+    await GqlRetry(ListUsers, { limit: 6, nextToken: this.state.nextToken }).then(({data}: any) => {
+      this.handleAddUsers(data.listUsers.items);
+      this.setState({ nextToken: data.listUsers.nextToken });
+    });
+  };
+
   render() {
     return (
       <div className="App">
@@ -36,26 +93,24 @@ class App extends Component {
         </header>
         <div className="App-content">
           <div className="Card-list">
-            <Connect query={graphqlOperation(ListUsers)}>
-              {/* TODO: Find the type for the graphql operation response */}
-              {(response: any) => {
-                if (response.loading || response.data === undefined || response.data === null || response.data.listUsers === undefined) return []               
-                return (
-                  response.data.listUsers.items.map((user: User, index: any) => <Card key={index} user={{
-                    id: user.id,
-                    name: user.name,
-                    avatar: user.avatar,
-                    address: user.address,
-                    description: user.description,
-                    createdAt: user.createdAt,
-                  }} />)
-                )
-              }}
-            </Connect>
+            {this.state.users.map((user: User, index: any) => (
+              <Card
+                key={index}
+                user={{
+                  id: user.id,
+                  name: user.name,
+                  avatar: user.avatar,
+                  address: user.address,
+                  description: user.description,
+                  createdAt: user.createdAt,
+                }}
+              />
+            ))}
           </div>
-          <LoadMoreButton />
+          <div className="Load-more-button-container">
+            { this.state.nextToken ? <button onClick={this.getNextUsers}>Load More...</button> : null }
+          </div>
         </div>
-        <div id="main"></div>
       </div>
     );
   }
--- src/tests/LoadMoreButton.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../App';
-
-test('renders load more button', () => {
-  const { getByText } = render(<App />);
-  const loadMoreButton = getByText("Load More...");
-  expect(loadMoreButton).toBeInTheDocument();
-});
