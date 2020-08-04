793a376f5d02da18e2aa2a2bee59093c105f7123
Rodrigo Cardenas <rcardenas@utexas.edu>
2020-08-04 05:06:27 -0500

    Able to infinitely load users and dynamically display them. Remove Load More Button when no users left to be fetched.

--- frontend/src/features/UserList/UserList.jsx
+++ frontend/src/features/UserList/UserList.jsx
@@ -1,23 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
 
+import Button from "../../components/Button/Button";
 import UserCard from "../UserCard/UserCard";
 
 const UserList = (props) => {
-  const [usersList, setUsersList] = useState(props.users);
-  const [nextToken, setNextToken] = useState(props.token);
-
-  console.log(nextToken);
   return (
     <React.Fragment>
-      {usersList.map((user) => {
-        return (
-          <UserCard
-            key={user.id}
-            onPencilClick={props.onEditClick}
-            user={user}
-          />
-        );
-      })}
+      <div className="middle-row">
+        {props.users.map((user) => {
+          return (
+            <UserCard
+              key={user.id}
+              onPencilClick={props.onEditClick}
+              user={user}
+            />
+          );
+        })}
+      </div>
+      {props.showLoadMore ? (
+        <div className="bottom-row">
+          <Button variant="primary" onClick={props.onLoadMore}>
+            Load More
+          </Button>
+        </div>
+      ) : null}
     </React.Fragment>
   );
 };
--- frontend/src/views/DashboardView.jsx
+++ frontend/src/views/DashboardView.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Query } from "react-apollo";
 
-import Button from "../components/Button/Button";
 import Input from "../components/Input/Input";
 import Typography from "../components/Typography/Typography";
 import UserList from "../features/UserList/UserList";
@@ -14,18 +13,54 @@ import "./Dashboard.css";
 const DashboardView = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [activeUser, setActiveUser] = useState(null);
+  const [areThereUsersLeft, setAreThereUsersLeft] = useState(true);
 
   const users = () => (
     <Query query={listUsers} variables={{ limit: 6 }}>
-      {({ loading, error, data }) => {
+      {({ loading, error, data, fetchMore }) => {
         if (loading) return <WatchLoader />;
         if (error) return `Error! ${error.message}`;
 
         return (
           <UserList
+            showLoadMore={areThereUsersLeft}
             users={data.listUsers.items}
-            token={data.listUsers.nextToken}
             onEditClick={onEditClick}
+            onLoadMore={() => {
+              fetchMore({
+                query: listUsers,
+                variables: { nextToken: data.listUsers.nextToken, limit: 3 },
+                updateQuery: (previousResult, { fetchMoreResult }) => {
+                  const previousUsers = previousResult.listUsers.items;
+                  const newUsers = fetchMoreResult.listUsers.items;
+                  const newCursor = fetchMoreResult.listUsers.nextToken;
+                  const lastCursor = previousResult.listUsers.nextToken;
+                  const allUsersSoFar = previousUsers.concat(newUsers);
+
+                  setAreThereUsersLeft(newCursor);
+
+                  if (lastCursor) {
+                    const newListUsers = {
+                      listUsers: {
+                        items: allUsersSoFar,
+                        nextToken: newCursor,
+                        __typename: "UserConnection",
+                      },
+                    };
+                    return newListUsers;
+                  } else {
+                    const allListUsers = {
+                      listUsers: {
+                        items: previousUsers,
+                        nextToken: lastCursor,
+                        __typename: "UserConnection",
+                      },
+                    };
+                    return allListUsers;
+                  }
+                },
+              });
+            }}
           />
         );
       }}
@@ -49,12 +84,7 @@ const DashboardView = () => {
             <Typography variant="h1">Users List</Typography>
             <Input placeholder="Search..." />
           </div>
-
-          <div className="middle-row">{users()}</div>
-
-          <div className="bottom-row">
-            <Button variant="primary">Load More</Button>
-          </div>
+          {users()}
         </div>
       </div>
       <UserModal open={isModalOpen} closeModal={closeModal} user={activeUser} />
