fd255b96b19a7f1bed92aea7fc9d9af14fd1861e
Bruno Cassapula <bruno.cassapula@outlook.com>
2020-11-30 16:16:33 -0300

    feat: including generic modal with animation

---
+++ web-app/src/app/modules/components/dialog/dialog.component.ts
@@ -0,0 +1,31 @@
+import { trigger, style, animate, transition } from '@angular/animations';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss'],
+  animations: [
+    trigger('dialog', [
+      transition('void => *', [
+        style({ transform: 'scale3d(.3, .3, .3)' }),
+        animate(100)
+      ]),
+      transition('* => void', [
+        animate(100, style({ transform: 'scale3d(.0, .0, .0)' }))
+      ])
+    ])
+  ]
+})
+export class DialogComponent {
+  @Input() closable = true;
+  @Input() visible: boolean;
+  @Output() visibleChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  close(): void {
+    this.visible = false;
+    this.visibleChange.emit(this.visible);
+  }
+}
