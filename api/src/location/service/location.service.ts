0489f2fa20ec9efd85c96603e716e4fd57e20530
Bruno Cassapula <bruno.cassapula@outlook.com>
2020-12-01 01:29:59 -0300

    fix: correcting the order of the geo parameters (for location)

--- api/src/location/service/location.service.ts
+++ api/src/location/service/location.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import mapboxGeocodeServiceFactory, { GeocodeService } from '@mapbox/mapbox-sdk/services/geocoding';
+import mapboxGeocodeServiceFactory, {
+  GeocodeService,
+} from '@mapbox/mapbox-sdk/services/geocoding';
 
 @Injectable()
 export class LocationService {
@@ -7,22 +9,27 @@ export class LocationService {
 
   constructor() {
     this.geocodeService = mapboxGeocodeServiceFactory({
-      accessToken: process.env.MAPBOX_API as string
+      accessToken: process.env.MAPBOX_API as string,
     });
   }
 
   public async getGeocodeCoordinates(address: string) {
-    const location = await this.geocodeService.forwardGeocode({
-      query: address,
-      mode: 'mapbox.places',
-      limit: 1
-    }).send()
+    const location = await this.geocodeService
+      .forwardGeocode({
+        query: address,
+        mode: 'mapbox.places',
+        limit: 1,
+      })
+      .send();
 
     const feat = location.body?.features;
     const coordinates = feat && Array.isArray(feat) && feat[0]?.center;
     const coordinatesFound = coordinates?.length === 2;
-    if (!coordinatesFound) throw 'Did not found any coordinate for the following address: ' + address;
+    if (!coordinatesFound)
+      throw (
+        'Did not found any coordinate for the following address: ' + address
+      );
 
-    return { lat: coordinates[0], long: coordinates[1] };
+    return { long: coordinates[0], lat: coordinates[1] };
   }
 }
