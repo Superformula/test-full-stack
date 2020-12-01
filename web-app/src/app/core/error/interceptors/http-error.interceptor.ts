980a2cdf6f4b17e8446150ca68e1c571d9bf0a31
Bruno Cassapula <bruno.cassapula@outlook.com>
2020-12-01 15:29:02 -0300

    fix: include a specific amount of retries in the interceptor

--- web-app/src/app/core/error/interceptors/http-error.interceptor.ts
+++ web-app/src/app/core/error/interceptors/http-error.interceptor.ts
@@ -16,7 +16,7 @@ export class HttpErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request)
       .pipe(
-        // retry(1),
+        retry(1),
         catchError((error: HttpErrorResponse) => {
           this.error.openErrorMessage();
           return throwError(error);
