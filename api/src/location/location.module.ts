1ac82201399d47d63eba439b5eda840ba4f5874f
Bruno Cassapula <bruno.cassapula@outlook.com>
2020-12-01 02:32:08 -0300

    refactor: removing unecessary location controller

--- api/src/location/location.module.ts
+++ api/src/location/location.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 
 import { LocationResolver } from './resolver/location.resolver';
 import { LocationService } from './service/location.service';
-import { LocationController } from './controller/location.controller';
 
 @Module({
   providers: [LocationResolver, LocationService],
-  controllers: [LocationController],
 })
-export class LocationModule { }
+export class LocationModule {}
