57766ead79e93605d263f1ac87ff6e406172b0cb
SF Applicant 4437 <sf.applicant.4437@gmail.com>
2020-12-14 02:11:32 -0300

    Implemented store functions

--- frontend/src/store/userList/reducer.ts
+++ frontend/src/store/userList/reducer.ts
@@ -7,9 +7,20 @@ const initialState: UserListState = {
 export default function userListReducer(state = initialState, action: UserListAction): UserListState {
   switch (action.type) {
     case 'ADD_USER':
+      return { ...state, users: [...state.users, action.payload] };
     case 'DELETE_USER':
+      return { ...state, users: state.users.filter((u) => u.id !== action.payload.userId) };
     case 'UPDATE_USER':
+      return {
+        ...state,
+        users: state.users.map((u) => {
+          if (u.id !== action.payload.id) return u;
+
+          return { ...u, ...action.payload };
+        }),
+      };
     case 'UPDATE_LIST':
+      return { ...state, users: action.payload };
     default:
       return state;
   }
