9637f213897807e69827b55ce25f464f436c6e77
SF Applicant 4437 <sf.applicant.4437@gmail.com>
2020-12-14 04:37:57 -0300

    Managed to grow the header container to kind of fit the parent width

--- frontend/src/components/UsersPage.tsx
+++ frontend/src/components/UsersPage.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import UserList from './UserList/UserList';
+import styles from './UsersPage.module.scss';
 
 const UsersPage: React.FC = () => {
   console.log('stop collapsing me');
 
-  return <UserList />;
+  return (
+    <>
+      <div className={styles.header}>
+        <h1>Ola</h1>
+        <input />
+      </div>
+
+      <UserList />
+    </>
+  );
 };
 export default UsersPage;
