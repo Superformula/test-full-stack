18056c3490c61e1c14aa337e26a79630c6f24eaf
SF Applicant 4437 <sf.applicant.4437@gmail.com>
2020-12-14 06:06:48 -0300

    Added debounce on the search term to prevent the API from being fired...
    ... on every key stroke

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
+import { QueryParamProvider } from 'use-query-params';
 import styles from './App.module.scss';
 import UsersPage from './components/UsersPage';
 import buildAppSyncApolloClient from './api/buildAppSyncApolloClient';
@@ -13,19 +14,21 @@ const client = buildAppSyncApolloClient();
 
 const App = (): JSX.Element => (
   <>
-    <ApolloProvider client={client}>
-      <Provider
-        store={createStore(
-          Reducers,
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any,no-underscore-dangle
-          (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
-        )}
-      >
-        <div className={styles.App}>
-          <UsersPage />
-        </div>
-      </Provider>
-    </ApolloProvider>
+    <QueryParamProvider>
+      <ApolloProvider client={client}>
+        <Provider
+          store={createStore(
+            Reducers,
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any,no-underscore-dangle
+            (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
+          )}
+        >
+          <div className={styles.App}>
+            <UsersPage />
+          </div>
+        </Provider>
+      </ApolloProvider>
+    </QueryParamProvider>
   </>
 );
 
--- frontend/src/components/SearchBar/SearchBar.tsx
+++ frontend/src/components/SearchBar/SearchBar.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { useQueryParam, StringParam } from 'use-query-params';
+import { useDebounceCallback } from '@react-hook/debounce';
 import styles from './SearchCard.module.scss';
 
 const SearchBar: React.FC = () => {
-  console.log('stop collapsing me!');
-  return <input placeholder="Search..." className={styles.input} />;
+  const [search, setSearch] = useState('');
+
+  const [, setQuerySearchTerm] = useQueryParam('searchTerm', StringParam);
+
+  const setQueryCallback = useCallback((searchTerm: string) => setQuerySearchTerm(searchTerm), [setQuerySearchTerm]);
+  const debouncedSetQuery = useDebounceCallback(setQueryCallback, 500);
+
+  const onInputChange = useCallback(
+    (evt: React.ChangeEvent<HTMLInputElement>) => {
+      setSearch(evt.target.value);
+      debouncedSetQuery(evt.target.value);
+    },
+    [setSearch, debouncedSetQuery],
+  );
+
+  return <input placeholder="Search..." className={styles.input} value={search} onChange={onInputChange} />;
 };
 
-export default SearchBar;
+export default React.memo(SearchBar);
