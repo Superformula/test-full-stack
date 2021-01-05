ed951944b20fa43503574082789905b2c3ed0d53
Daniel Miranda <dmiranda2791@gmail.com>
2021-01-05 10:20:19 -0600

    Add on mouse over effects

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -1,6 +1,7 @@
 import Avatar from "./Avatar";
 import styles from "./Card.module.css";
 import { format } from "date-fns";
+import { useState } from "react";
 
 interface User {
   name: string;
@@ -15,15 +16,31 @@ interface Props {
   item: User;
 }
 const Card = ({ item }: Props) => {
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
+  const toggleIsMouseOver = () => {
+    setIsMouseOver(!isMouseOver);
+  };
+
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      onMouseOver={toggleIsMouseOver}
+      onMouseOut={toggleIsMouseOver}
+    >
+      {isMouseOver ? (
+        <img className={styles.icon} src="/icons/edit.svg" />
+      ) : null}
       <Avatar />
       <div className={styles.body}>
         <h2 className={styles.username}>{item.name}</h2>
         <p className={styles.description}>{item.description}</p>
-        <div className={styles.meta}>
-          created <span>{format(new Date(item.createdAt), "dd MMM YYY")}</span>
-        </div>
+        {isMouseOver ? (
+          <div className={styles.meta}>
+            created{" "}
+            <span>{format(new Date(item.createdAt), "dd MMM YYY")}</span>
+          </div>
+        ) : null}
       </div>
     </div>
   );
