4a8a9ba91ded2c40fbb877984c2437ea95074169
Daniel Miranda <dmiranda2791@gmail.com>
2021-01-05 11:49:04 -0600

    Responsive card and UsersList grid

--- frontend/src/components/UsersList/UsersList.tsx
+++ frontend/src/components/UsersList/UsersList.tsx
@@ -1,6 +1,8 @@
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/client";
 import Card from "@components/Card";
+import styles from "./UsersList.module.css";
+
 import { listUsers } from "../../../graphql/queries";
 
 const UsersList = () => {
@@ -12,7 +14,7 @@ const UsersList = () => {
       listUsers: { items },
     } = data;
     return (
-      <div>
+      <div className={styles.grid}>
         {items.map((item) => (
           <Card key={item.id} item={item} />
         ))}
