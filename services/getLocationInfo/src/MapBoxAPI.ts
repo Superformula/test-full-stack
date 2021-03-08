1c6074f8d1019c76bffb610074beb09355ea6dd0
ricardo <rleon@devsu.com>
2021-03-08 10:45:28 -0500

    Tested basic behavior

--- services/getLocationInfo/src/MapBoxAPI.ts
+++ services/getLocationInfo/src/MapBoxAPI.ts
@@ -1,4 +1,4 @@
-import Fetch, { RequestInfo } from 'node-fetch';
+import Fetch from 'node-fetch';
 import { LocationAPI, LocationBasicData } from './location';
 
 if (!process.env.MAPBOX_ACCESS_TOKEN) {
@@ -11,25 +11,21 @@ const FORWARD_GEOCODING_SERVICE_URL = 'geocoding/v5/mapbox.places';
 export class MapBoxAPI implements LocationAPI {
   async getLocationData(rawAddress: string):Promise<LocationBasicData[]> {
     const url = `${BASE_URL}/${FORWARD_GEOCODING_SERVICE_URL}`;
-    const urlWithAddress = `${url}/${encodeURI(rawAddress)}?access_token=${process.env.MAPBOX_ACCESS_TOKEN}&autocomplete=true`;
-    const response = await Fetch({ url: urlWithAddress } as RequestInfo);
+    const urlWithAddress = `${url}/${encodeURIComponent(rawAddress)}.json?access_token=${process.env.MAPBOX_ACCESS_TOKEN}&autocomplete=true`;
+    const response = await Fetch(urlWithAddress);
     const data = await response.json();
     const locations:LocationBasicData[] = [];
-    try {
-      const features = data.features || [];
-      features.slice(0, 10).forEach((feature:any) => {
-        const properties = feature.properties || {};
-        locations.push({
-          name: properties.place_name,
-          category: properties.category,
-          isLandmark: !!properties.landmark,
-          address: properties.address,
-          coordinates: feature.center
-        });
+    const features = data.features || [];
+    features.slice(0, 10).forEach((feature:any) => {
+      const properties = feature.properties || {};
+      locations.push({
+        name: properties.place_name,
+        category: properties.category,
+        isLandmark: !!properties.landmark,
+        address: properties.address,
+        coordinates: feature.center
       });
-    } catch (err) {
-      console.log(err);
-    }
+    });
     return locations;
   }
 }
--- services/getLocationInfo/src/index.ts
+++ services/getLocationInfo/src/index.ts
@@ -1,14 +1,15 @@
-import { LocationAPI } from './location';
 import { MapBoxAPI } from './MapBoxAPI';
 
-class GetLocationDataService {
-  constructor(private locationService:LocationAPI) {
-  }
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async handler(event:any, _context:any) {
-    const { address } = event;
-    return this.locationService.getLocationData(address);
-  }
-}
+const LocationService = new MapBoxAPI();
 
-export default new GetLocationDataService(new MapBoxAPI());
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const handler = async (event:any, _context:any) => {
+  const { address } = (event.arguments || {});
+  try {
+    const locations = await LocationService.getLocationData(address);
+    return locations;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+};
