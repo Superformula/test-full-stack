bdf0795d6c3e6da2b933f167e2be6ca2943ae77f
Nick Plekhanov <nickatgit@yandex.com>
2021-03-26 13:51:32 +0300

    Tweak linting settings

--- webclient/.eslintrc.js
+++ webclient/.eslintrc.js
@@ -1,3 +1,6 @@
+const OFF = 0
+const ERROR = 2
+
 module.exports = {
   env: {
     browser: true,
@@ -26,24 +29,36 @@ module.exports = {
     }
   },
   rules: {
-    'react/react-in-jsx-scope': 0,
-    'react/display-name': 0,
-    'react/prop-types': 0,
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/explicit-member-accessibility': 0,
-    '@typescript-eslint/indent': 0,
-    '@typescript-eslint/member-delimiter-style': 0,
-    '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/no-var-requires': 0,
-    '@typescript-eslint/no-use-before-define': 0,
+    indent: [ERROR, 2, { SwitchCase: 1 }],
+    'linebreak-style': [ERROR, 'unix'],
+    curly: [ERROR, 'all'],
+    'jsx-quotes': [ERROR, 'prefer-double'],
+    quotes: [ERROR, 'single', { allowTemplateLiterals: true }],
+    semi: [ERROR, 'never'],
+    'arrow-parens': [ERROR, 'as-needed'],
+    'require-await': ERROR,
+    'capitalized-comments': [
+      'error',
+      'always',
+      { ignoreConsecutiveComments: true }
+    ],
+    'arrow-body-style': [ERROR, 'as-needed'],
+    'no-implicit-coercion': ERROR,
+    'react/react-in-jsx-scope': OFF,
+    'react/prop-types': OFF,
+    '@typescript-eslint/explicit-function-return-type': OFF,
+    '@typescript-eslint/explicit-member-accessibility': OFF,
+    '@typescript-eslint/member-delimiter-style': OFF,
+    '@typescript-eslint/no-explicit-any': OFF,
+    '@typescript-eslint/no-use-before-define': OFF,
     '@typescript-eslint/no-unused-vars': [
-      2,
+      ERROR,
       {
         argsIgnorePattern: '^_'
       }
     ],
     'no-console': [
-      2,
+      ERROR,
       {
         allow: ['warn', 'error']
       }
