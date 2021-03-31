7b187209667fff4b4bc89321bbab5a9d65ca054a
Nick Plekhanov <nickatgit@yandex.com>
2021-03-31 20:05:44 +0300

    Fix an error with a Function Component requiring a ref attr

--- webclient/components/Users/UserCard.tsx
+++ webclient/components/Users/UserCard.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react'
+import { ReactElement, ReactNode, forwardRef } from 'react'
 import Link from 'next/link'
 
 import Avatar from '../generic/Avatar'
@@ -9,18 +9,35 @@ import { formatDate } from '../../utils/date-helpers'
 
 import styles from './UserCard.module.css'
 
+interface RefProps {
+  children: ReactNode;
+  onClick?: () => void;
+  href?: string;
+}
+
 interface Props {
   user: User;
   onUserDelete: (userId: string) => Promise<void>;
 }
 
 export default function UserCard({ user, onUserDelete }: Props): ReactElement {
+  const LinkChildWrapper = forwardRef<HTMLAnchorElement, RefProps>(function LinkChildWrapper({ children, onClick, href }: { children: ReactNode, onClick: () => void, href: string }, ref) {
+    return (
+      <a href={href} onClick={onClick} ref={ref}>
+        {children}
+      </a>
+    )
+  })
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.actionButtonsWrapper}>
         <span className={styles.actionButton} title="Edit User">
-          <Link as={`/users/${user.id}`} href={`/?userId=${user.id}`}>
-            <EditIcon />
+          {/* Ref: https://nextjs.org/docs/api-reference/next/link#if-the-child-is-a-function-component */}
+          <Link as={`/users/${user.id}`} href={`/?userId=${user.id}`} passHref>
+            <LinkChildWrapper>
+              <EditIcon />
+            </LinkChildWrapper>
           </Link>
         </span>
         <span
