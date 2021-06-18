7f7b23fc97dde9fff811c33e2f572d161557fb87
Marcelo Aleixo da Silva <marcelloaleixo@gmail.com>
2021-06-18 16:22:17 -0300

    feat: create input component

---
+++ src/components/input/Input.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import { Input, InputProps } from './Input';
+
+export default {
+  title: 'Input',
+  component: Input,
+} as Meta;
+
+const Template: Story = (args:InputProps) => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <Input {...args} />
+);
+
+export const Primary = Template.bind({});
+
+export const InputWithPlaceholder = Template.bind({});
+InputWithPlaceholder.args = {
+  placeholder: 'Placeholder',
+};
+
+export const InputWithLabel = Template.bind({});
+InputWithLabel.args = {
+  label: 'Label',
+  placeholder: 'Placeholder',
+};
---
+++ src/components/input/Input.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Input.scss';
+
+export interface InputProps {
+  placeholder?: string
+  label?: string
+  name?: string
+  onChange?: () => void
+  onBlur?: () => void
+  ref?: React.Ref<any>
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export const Input: React.FC<InputProps> = ({
+  name,
+  label,
+  placeholder,
+  onChange,
+  onBlur,
+  ref,
+}) => (
+  <div className="input-wrapper">
+    <label htmlFor={name}>{label}</label>
+    <input
+      className="input-field"
+      name={name}
+      placeholder={placeholder}
+      onChange={onChange}
+      onBlur={onBlur}
+      ref={ref}
+    />
+  </div>
+);
