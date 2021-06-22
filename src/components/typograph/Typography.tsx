3f1bc348b5879ebf943b3f7bb28e1c8a32ccc1f7
Marcelo Aleixo da Silva <marcelloaleixo@gmail.com>
2021-06-22 19:49:09 -0300

    fix: map the text tag to the Typography component

--- src/components/typograph/Typography.tsx
+++ src/components/typograph/Typography.tsx
@@ -6,19 +6,29 @@ type VariantEnum = 'h1' | 'h2' | 'paragraph';
 type ColorEnum = 'primary' | 'error';
 
 interface CardProps {
-  variant?: VariantEnum
+  variant: VariantEnum
   color?: ColorEnum
   className?: string
 }
 
+const variantMapping = {
+  h1: 'h1',
+  h2: 'h2',
+  paragraph: 'p',
+};
+
 export const Typography: React.FC<CardProps> = ({
   children, variant, className, color = 'primary',
-}) => (
-  <div className={classnames(className, {
-    [`typography--variant-${variant}`]: variant,
-    [`typography--color-${color}`]: color,
-  })}
-  >
-    {children}
-  </div>
-);
+}) => {
+  const Component: any = variantMapping[variant];
+
+  return (
+    <Component className={classnames(className, 'typography', {
+      [`typography--variant-${variant}`]: variant,
+      [`typography--color-${color}`]: color,
+    })}
+    >
+      {children}
+    </Component>
+  );
+};
